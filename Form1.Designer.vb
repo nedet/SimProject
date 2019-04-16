dca0b1c4034ae0b909ac2897a75606085d6e43f2
jeremy antonuccio <nedetheonly@gmail.com>
2019-04-15 23:35:49 -0400

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,125 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.PicA = New System.Windows.Forms.PictureBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.tmrA = New System.Windows.Forms.Timer(Me.components)
+        Me.Btnstop = New System.Windows.Forms.Button()
+        Me.RadSQ = New System.Windows.Forms.RadioButton()
+        Me.radCir = New System.Windows.Forms.RadioButton()
+        Me.Label1 = New System.Windows.Forms.Label()
+        CType(Me.PicA, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PicA
+        '
+        Me.PicA.Image = CType(resources.GetObject("PicA.Image"), System.Drawing.Image)
+        Me.PicA.Location = New System.Drawing.Point(13, 55)
+        Me.PicA.Name = "PicA"
+        Me.PicA.Size = New System.Drawing.Size(715, 464)
+        Me.PicA.TabIndex = 0
+        Me.PicA.TabStop = False
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(35, 13)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(37, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "start"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'tmrA
+        '
+        Me.tmrA.Interval = 500
+        '
+        'Btnstop
+        '
+        Me.Btnstop.Location = New System.Drawing.Point(78, 13)
+        Me.Btnstop.Name = "Btnstop"
+        Me.Btnstop.Size = New System.Drawing.Size(38, 23)
+        Me.Btnstop.TabIndex = 3
+        Me.Btnstop.Text = "stop"
+        Me.Btnstop.UseVisualStyleBackColor = True
+        '
+        'RadSQ
+        '
+        Me.RadSQ.AutoSize = True
+        Me.RadSQ.Location = New System.Drawing.Point(139, 13)
+        Me.RadSQ.Name = "RadSQ"
+        Me.RadSQ.Size = New System.Drawing.Size(87, 17)
+        Me.RadSQ.TabIndex = 4
+        Me.RadSQ.TabStop = True
+        Me.RadSQ.Text = "Draw Square"
+        Me.RadSQ.UseVisualStyleBackColor = True
+        '
+        'radCir
+        '
+        Me.radCir.AutoSize = True
+        Me.radCir.Location = New System.Drawing.Point(139, 37)
+        Me.radCir.Name = "radCir"
+        Me.radCir.Size = New System.Drawing.Size(79, 17)
+        Me.radCir.TabIndex = 5
+        Me.radCir.TabStop = True
+        Me.radCir.Text = "Draw Circle"
+        Me.radCir.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(514, 22)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Label1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(740, 531)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.radCir)
+        Me.Controls.Add(Me.RadSQ)
+        Me.Controls.Add(Me.Btnstop)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.PicA)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PicA, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PicA As PictureBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents tmrA As Timer
+    Friend WithEvents Btnstop As Button
+    Friend WithEvents RadSQ As RadioButton
+    Friend WithEvents radCir As RadioButton
+    Friend WithEvents Label1 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,128 @@
+﻿Public Class Form1
+
+
+    Dim x(7, 3) As Integer
+    Dim y(7, 7) As Integer
+    Dim Vy(7) As Integer
+    Dim Vx(7) As Integer
+    Dim bc, PixelColor As Color
+
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        bc = Me.BackColor
+        For f As Integer = 1 To 7
+            x(f, 3) = 10
+            x(f, 2) = 20
+            x(f, 1) = 30
+            x(f, 0) = 40
+        Next
+        For L As Integer = 0 To 4
+            y(1, L) = 180
+            y(2, L) = 200
+            y(3, L) = 220
+            y(4, L) = 240
+            y(5, L) = 260
+            y(6, L) = 280
+            y(7, L) = 300
+        Next
+        Vy(1) = -0
+        Vy(2) = -0
+        Vy(3) = -0
+        Vy(4) = 0
+        Vy(5) = 0
+        Vy(6) = 0
+        Vy(7) = 0
+        Vx(1) = 10
+        Vx(2) = 10
+        Vx(3) = 10
+        Vx(4) = 10
+        Vx(5) = 10
+        Vx(6) = 10
+        Vx(7) = 10
+
+    End Sub
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        tmrA.Start() 'starts timer
+
+
+    End Sub
+
+    Private Sub tmrA_Tick(sender As Object, e As EventArgs) Handles tmrA.Tick
+        Dim penA As New Pen(bc)
+        With PicA.CreateGraphics
+            For K As Integer = 0 To 3
+                ' draws dot that goes across pic box
+                .DrawEllipse(Pens.Black, x(1, K), y(1, K), 2, 2)
+                .DrawEllipse(Pens.Black, x(2, K), y(2, K), 2, 2)
+                .DrawEllipse(Pens.Black, x(3, K), y(3, K), 2, 2)
+                .DrawEllipse(Pens.Black, x(4, K), y(4, K), 2, 2)
+                .DrawEllipse(Pens.Black, x(5, K), y(5, K), 2, 2)
+                .DrawEllipse(Pens.Black, x(6, K), y(6, K), 2, 2)
+                .DrawEllipse(Pens.Black, x(7, K), y(7, K), 2, 2)
+
+            Next
+            For j As Integer = 1 To 7
+                .DrawEllipse(penA, x(j, 3), y(j, 3), 2, 2) 'covers last dots
+            Next
+
+            For M As Integer = 1 To 7
+                x(M, 3) = x(M, 2)
+                x(M, 2) = x(M, 1)
+                x(M, 1) = x(M, 0)
+                x(M, 0) += Vx(M)
+                y(M, 3) = y(M, 2) ' makes lines curve 
+                y(M, 2) = y(M, 1)
+                y(M, 1) = y(M, 0)
+                y(M, 0) += Vy(M)
+
+                If x(M, 0) > Me.Width Then
+                    x(M, 0) = 0
+                End If
+
+            Next M
+
+            Dim Pict1 As Object
+
+
+            ' the color is obtained from the top left (bx,by) of the picture
+
+            Pict1 = PicA.Image
+
+            PixelColor = Pict1.getpixel(x(4, 0), y(4, 0))    'usual case gets actual color
+            If PixelColor.ToArgb = Color.Black.ToArgb Then
+                Label1.Text = "black"
+                Vx(4) *= -1
+                If x(4, 0) < 0 Then
+                    Vx(4) *= -1
+                End If
+            End If
+
+
+
+            If RadSQ.Checked = True Then
+                PicA.CreateGraphics.DrawEllipse(penA, 310, 200, 80, 80)         'erases other object and draws new object
+                PicA.CreateGraphics.DrawRectangle(Pens.Red, 310, 200, 80, 80)
+            ElseIf radCir.Checked = True Then
+                PicA.CreateGraphics.DrawRectangle(penA, 310, 200, 80, 80)
+                PicA.CreateGraphics.DrawEllipse(Pens.Red, 310, 200, 80, 80)
+            End If
+
+
+
+
+
+
+
+        End With
+    End Sub
+
+
+
+    Private Sub Btnstop_Click(sender As Object, e As EventArgs) Handles Btnstop.Click
+        tmrA.Stop() 'stops timer
+
+    End Sub
+End Class
